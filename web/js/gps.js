2e4e3cb16fc683869e7926e5f23584fd7694b4f1
Dean Shub <deanshub@gmail.com>
2013-11-14 15:48:17 +0200

    adding jquery

--- web/js/gps.js
+++ web/js/gps.js
@@ -15,12 +15,29 @@ lbd.config(function ($routeProvider) {
 var controllers = {};
 
 controllers.gpsCtrl = function ($scope) {
+        $scope.markers=[];
         $scope.people = [
                 {id:"1",firstName:"dean",lastName:"shub",city:"rishon le zion"},
                 {id:"2",firstName:"lior",lastName:"bentov",city:"tel aviv"},
                 {id:"3",firstName:"yotam",lastName:"roth",city:"rehovot"},
                 {id:"4",firstName:"shirit",lastName:"vaxman",city:"rishon le zion"},
                 {id:"5",firstName:"talya",lastName:"ronen",city:"beer sheva"}];
+
+        $scope.addMarker = function (){
+                $scope.markers.push({
+                        latitude: parseFloat($scope.markerLat),
+                        longitude: parseFloat($scope.markerLng)
+                });
+                        
+                $scope.markerLat = null;
+                $scope.markerLng = null;
+
+                 var marker = new google.maps.Marker({
+                 position: myLatlng,
+                 map: map,
+                 title: 'Hello World!'
+                });
+        }
 };
 
 lbd.controller(controllers);
