3d89df3907776ab7cafc5329dc4cb2fd0f5e96f0
Yaniv Israel <isyaniv@gmail.com>
2013-11-14 23:54:24 +0200

    modify server

---
+++ server/server2.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+// New Code
+// var mongo = require('mongodb').mongo;
+// mongo.connect('localhost:27017/test');
+//var monk = require('monk');
+//var db= mongo.db( 'test', new mongo.Server( 'localhost', 27017, {}), {w:0});
+//var db = mongo('mongodb://127.0.0.1:27017/test');
+var db = require('mongoskin').db('localhost:27017/test');
+// var mongoose = require('mongoose');
+// var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
+// var dataSchema = mongoose.Schema({}, { collection: 'reports' });
+// var modReport = mongoose.model('ReportModel'.dataSchema);
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// app.get('/', routes.index);
+// app.get('/users', user.list);
+// app.get('/helloworld', routes.helloworld);
+app.get('/reports', get_reports(db));
+app.get('/diags', get_diags(db));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+function get_reports(db) {
+    return function(req, res) {
+    	//console.log(db.collection('reports').distinct('diag.name'));
+    	db.collection('reports').find().toArray(function(err, result) {
+		    if (err) throw err;
+		    res.send(result);
+		})
+		//var collection = db.get("reports");	  
+	    //collection.find({},{limit:20},function(e,docs){
+	    //	res.json(docs);
+	    //})
+    };
+};
+
+function get_diags(db) {
+    return function(req, res) {
+    	db.collection('reports').distinct("diag.name", {}, function(err,docs) {
+    		res.send(docs);
+    	});
+    };
+};
