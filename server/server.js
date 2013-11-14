e9e2c2e5d1f49d1a4354e14afb94c81a0f1ffea2
Yaniv Israel <isyaniv@gmail.com>
2013-11-14 17:30:04 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -13,16 +13,26 @@ app.set('port', process.env.PORT || 3000);
 // app.get('/', routes.index);
 // app.get('/users', user.list);
 // app.get('/helloworld', routes.helloworld);
-app.get('/get_repors', get_repors(db));
+app.get('/get_reports', get_reports(db));
+app.get('/get_diags', get_diags(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-function get_repors(db) {
+function get_reports(db) {
     return function(req, res) {
 		var collection = db.get("reports");	  
 	    collection.find({},{limit:20},function(e,docs){
+	    	res.json(docs);
+	    })
+    };
+};
+
+function get_diags(db) {
+    return function(req, res) {
+		var collection = db.get("reports");	  
+	    collection.distinct('diags.name',function(e,docs){
 	    	res.json(docs);
 	    })
     };
